8d17d26d2ed7785c19a0e3381029e1eb5e9069dc
Sang-Yun Oh <syoh@users.noreply.github.com>
2020-07-07 07:45:34 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,12 +12,10 @@ openssl req -x509 -nodes \
     -out keys/mycert.pem \
     -subj "/C=US/ST=A/L=B/O=C/OU=D/CN=F"
 
-## The binary at ucsb.box.com (created using pyinstaller) hashes a password according to
-## https://jupyter-notebook.readthedocs.io/en/stable/public_server.html#preparing-a-hashed-password
-
-wget -nc https://ucsb.box.com/shared/static/0g291d3aw5fpl0j7j02kyrnm3zuutaqu \
-    -O hash_password && \
-    chmod u+x hash_password
+## download utility for hashing jupyter notebook password
+wget -nc https://github.com/dddlab/jupyter-passwd/releases/download/v0.0.1.6/hash-password \
+    -O .dddlab/hash-password && \
+    chmod u+x .dddlab/hash-password
 
 # https://github.com/docker/compose/issues/4223#issuecomment-280077263
 # https://docs.docker.com/compose/environment-variables/
@@ -28,5 +26,5 @@ cat <<EOF > .env
 
 HOST_DIR=${PWD}
 CERT_DIR=${PWD}/keys
-PASSWD=$(./hash_password)
+PASSWD=$(.dddlab/hash-password)
 EOF
